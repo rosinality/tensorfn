963d76729ba1f9e1b12a0ff68576c40f014ffa59
Kim Seonghyeon <kim.seonghyeon@snu.ac.kr>
2020-05-03 01:05:25 +0000

    New version

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 setup(
     name='tensorfn',
-    version='0.0.1',
+    version='0.1.0',
     description='Non-opionated utility library for PyTorch',
     url='https://github.com/rosinality/tensorfn',
     author='Kim Seonghyeon',
     author_email='kim.seonghyeon@navercorp.com',
     license='MIT',
 )
-
